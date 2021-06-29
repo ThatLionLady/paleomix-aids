3122f9ffbc2e985f2ff9a83b618ba2d5381479a6
Caitlin J. Curry <ThatLionLady@users.noreply.github.com>
2021-06-29 10:07:46 -0700

    Update paleomix-summary.sh

--- paleomix-summary.sh
+++ paleomix-summary.sh
@@ -3,6 +3,7 @@
 SAMPLES=$(<$1) 	# /PATH/TO/List-of-Samples.txt
 dir=$2 			# /PATH/OF/MAIN/DIRECTORY/
 sumdir=$3 		# /PATH/WITH/PALEOMIX/SUMMARIES/
+ref=$4			# reference name
 
 echo -e "Sample \t lib_type \t seq_reads_pairs \t seq_trash_pe_1 \t seq_trash_pe_1_frac \t seq_trash_pe_2 \t seq_trash_pe_2_frac \t seq_collapsed \t seq_collapsed_frac \t seq_retained_reads \t seq_retained_nts \t seq_retained_length \t hits_raw \t hits_raw_frac \t hits_clonality \t hits_unique \t hits_unique_frac \t hits_coverage \t hits_length" >> ${dir}PALEOMIX-summaries
 
@@ -18,13 +19,13 @@ for sample in $SAMPLES; do
 	seq_retained_reads=$(awk -v sample=${sample} '$3 == sample && $4 == "seq_retained_reads" {print $5}' ${sumdir}${sample}.summary)
 	seq_retained_nts=$(awk -v sample=${sample} '$3 == sample && $4 == "seq_retained_nts" {print $5}' ${sumdir}${sample}.summary)
 	seq_retained_length=$(awk -v sample=${sample} '$3 == sample && $4 == "seq_retained_length" {print $5}' ${sumdir}${sample}.summary)
-	hits_raw=$(awk -v sample=${sample} '$3 == sample && $4 == "hits_raw(PanLeo1.0)" {print $5}' ${sumdir}${sample}.summary)
-	hits_raw_frac=$(awk -v sample=${sample} '$3 == sample && $4 == "hits_raw_frac(PanLeo1.0)" {print $5}' ${sumdir}${sample}.summary)
-	hits_clonality=$(awk -v sample=${sample} '$3 == sample && $4 == "hits_clonality(PanLeo1.0)" {print $5}' ${sumdir}${sample}.summary)
-	hits_unique=$(awk -v sample=${sample} '$3 == sample && $4 == "hits_unique(PanLeo1.0)" {print $5}' ${sumdir}${sample}.summary)
-	hits_unique_frac=$(awk -v sample=${sample} '$3 == sample && $4 == "hits_unique_frac(PanLeo1.0)" {print $5}' ${sumdir}${sample}.summary)
-	hits_coverage=$(awk -v sample=${sample} '$3 == sample && $4 == "hits_coverage(PanLeo1.0)" {print $5}' ${sumdir}${sample}.summary)
-	hits_length=$(awk -v sample=${sample} '$3 == sample && $4 == "hits_length(PanLeo1.0)" {print $5}' ${sumdir}${sample}.summary)
+	hits_raw=$(awk -v sample=${sample} '$3 == sample && $4 == "hits_raw(${ref})" {print $5}' ${sumdir}${sample}.summary)
+	hits_raw_frac=$(awk -v sample=${sample} '$3 == sample && $4 == "hits_raw_frac(${ref})" {print $5}' ${sumdir}${sample}.summary)
+	hits_clonality=$(awk -v sample=${sample} '$3 == sample && $4 == "hits_clonality(${ref})" {print $5}' ${sumdir}${sample}.summary)
+	hits_unique=$(awk -v sample=${sample} '$3 == sample && $4 == "hits_unique(${ref})" {print $5}' ${sumdir}${sample}.summary)
+	hits_unique_frac=$(awk -v sample=${sample} '$3 == sample && $4 == "hits_unique_frac(${ref})" {print $5}' ${sumdir}${sample}.summary)
+	hits_coverage=$(awk -v sample=${sample} '$3 == sample && $4 == "hits_coverage(${ref})" {print $5}' ${sumdir}${sample}.summary)
+	hits_length=$(awk -v sample=${sample} '$3 == sample && $4 == "hits_length(${ref})" {print $5}' ${sumdir}${sample}.summary)
 
 	echo -e "${sample} \t $lib_type \t $seq_reads_pairs \t $seq_trash_pe_1 \t $seq_trash_pe_1_frac \t $seq_trash_pe_2 \t $seq_trash_pe_2_frac \t $seq_collapsed \t $seq_collapsed_frac \t $seq_retained_reads \t $seq_retained_nts \t $seq_retained_length \t $hits_raw \t $hits_raw_frac \t $hits_clonality \t $hits_unique \t $hits_unique_frac \t $hits_coverage \t $hits_length" >> ${dir}PALEOMIX-summaries
 done
